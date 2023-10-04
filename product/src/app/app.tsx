7e7f2451b8fd9ba17e73236754cb65bff364e065
AgentEnder <craigorycoppola@gmail.com>
2023-10-04 13:59:03 -0400

    feat: setup module federation

---
+++ product/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.scss';
+
+import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <div>
+      Hello World 2
+      <NxWelcome title="product" />
+    </div>
+  );
+}
+
+export default App;
